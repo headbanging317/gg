72330d7f019939e0634b9ed2b34d9b939d65dedc
headbanging317 <24thelf@gmail.com>
2017-10-10 17:44:15 +0900

    index.html written

--- search/views.py
+++ search/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'search/index.html')
+
+def result(request):
+    return render(request, 'search/result.html')
