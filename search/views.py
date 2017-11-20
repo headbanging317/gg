7319275e6dcadc9fc5459a0e5e7205248b8f1c9f
headbanging317 <24thelf@gmail.com>
2017-11-20 20:32:45 +0900

    add jobkorea result & content_j will fixed 37 to 36.8

--- search/views.py
+++ search/views.py
@@ -16,22 +16,35 @@ def result(request):
     keyWord = request.GET.get('keyWord')
     page = int(request.GET.get('page'))
     full2 = Full2.objects.all()
-    company = {}
-    content = {}
+    
+    # saramin list
+    company_s = []
+    content_s = []
+    day_s = []
+    detail_s = []
+
+    # jobkorea list
+    company_j = []
+    content_j = []
+    day_j = []
+    detail_j = []
+
     full = ['']
-    dic = {'name':company, 'content':content}
+    # dic = {'name':company, 'content':content}
   
     for i in range (1,page+1):
         s = str(i)
-        url = 'http://www.saramin.co.kr/zf_user/search/recruit/page/'+s+'?pageCount=30&multiLine=&searchword='+ keyWord +'&company_cd=1&area=&domestic=&oversee=&jobCategory=&jobType=&career=&order=&periodType=&period=&condition=&arange=&company=&employ=&rSearchword=&hSearchword=&hInclude=&hExcept=&searchType=search&correctionSearch='
-        r = get(url)
+        url_s = 'http://www.saramin.co.kr/zf_user/search/recruit/page/'+s+'?pageCount=30&multiLine=&searchword='+ keyWord +'&company_cd=1&area=&domestic=&oversee=&jobCategory=&jobType=&career=&order=&periodType=&period=&condition=&arange=&company=&employ=&rSearchword=&hSearchword=&hInclude=&hExcept=&searchType=search&correctionSearch='
+        saramin = get(url_s)
+        url_j = 'http://www.jobkorea.co.kr/Search/?stext='+keyWord
+        jobkorea = get(url_j)
         # url2 = 'http://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do?currentPage=1&rowPerPage=300'
         # url3 = url
         # html = requests.get(url3).text
         
 
-        soup = BeautifulSoup(r.content.decode('utf-8', 'replace'))
-        
+        soup_s = BeautifulSoup(saramin.content.decode('utf-8', 'replace'))
+        soup_j = BeautifulSoup(jobkorea.content.decode('utf-8', 'replace'))
         
         name = ''
         link = ''
@@ -40,27 +53,55 @@ def result(request):
         #     link += member_tag['href']
 
         title = ''
-        titles = soup.find_all('strong', id='articleBodyContents')
+        # titles = soup.find_all('strong', id='articleBodyContents')
 
+        # ---------- saramin scrapping---------- #
+        companies_s = soup_s.select('.company_inbox li div div h2 a')
+        contents_s = soup_s.select('.txt span a')
+        days_s = soup_s.select('.txt .day')
+        details_s = soup_s.select('.terms_li')
 
-        companies = soup.select('.company_inbox li div div h2 a')
-        contents = soup.select('.txt span a')
+        for detail_tag in details_s:
+            detail_s.append(detail_tag.text)
 
-        
+        for day_tag in days_s:
+            day_s.append(day_tag.text)
 
-        for title_tag in companies:
-            # company.append(title_tag.text)
-            company[title_tag.text] = title_tag.get('href')
+        for title_tag in companies_s:
+            company_s.append(title_tag.text)
+            # company[title_tag.text] = title_tag.get('href')
             
 
         # print (contents)
-        for content_tag in contents:
-            # content.append(content_tag.text)
-            content[content_tag.text] = content_tag.get('href')
+        for content_tag in contents_s:
+            content_s.append(content_tag.text)
+            # content[content_tag.text] = content_tag.get('href')
         # full = company + content
      
-    
-    context = {'url':url, 'company':company, 'content':content, 'keyWord':keyWord, 'full':full, 'dic':dic}
+
+        #---------- jobkorea scrapping----------#
+        companies_j = soup_j.select('.corpName .giTitle')
+        contents_j = soup_j.select('.gibInfo .devSpcfcGI')
+        days_j = soup_j.select('.devSpcfcGI .gibDay')
+        details_j = soup_j.select('.gibDesc .devSpcfcGI')
+
+        for title_tag in companies_j:
+            company_j.append(title_tag.text)
+
+        for detail_tag in details_j:
+            detail_j.append(detail_tag.text)
+
+        for content_tag in contents_j:
+            content_j.append(content_tag.text)
+
+        for day_tag in days_j:
+            day_j.append(day_tag.text)
+
+        print('assd')
+        print(company_j)
+    context = {'url':url_s, 'company_s':company_s, 'content_s':content_s, 'keyWord':keyWord, 'full':full, 'day_s':day_s, 'detail_s':detail_s, 
+                'company_j':company_j, 'content_j':content_j, 'detail_j':detail_j, 'day_j':day_j
+    }
     
   
     return render(request, 'search/result.html', context)
